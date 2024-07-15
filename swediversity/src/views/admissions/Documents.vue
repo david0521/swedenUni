bb1a5e78d5e24cec96ff41803972257543ef409e
David Hong <davidbor@net.chalmers.se>
2024-07-15 11:38:18 +0200

    #43 Required documents page  This commit will create a page to explain about the required documents for the admission process.

---
+++ swediversity/src/views/admissions/Documents.vue
@@ -0,0 +1,203 @@
+<template>
+    <div class="stage" style="background-color: #e3d5ca;">
+        <div class="fade-in">
+        <div class="title-section main-font">
+            <h1>시작하기 전에...</h1>
+        </div>
+        <div class="title-caption caption-font">
+            <p style="text-align: center;">
+                다음 설명은 대한민국에 있는 고등학교를 진학하고 졸업한 학생들에게만 해당되는 내용입니다. <br>
+                이하 서술되어 있는 모든 서류는 작성된 언어가 영어, 프랑스어, 스페인어, 독일어, 덴마크어, 스웨덴어, 핀란드어가 <br>
+                아닌 경우, 반드시 영어 혹은 스웨덴어로 전문 번역가에 의해 번역된 이후 법률대리인에게 공증 받아 제출되야합니다. <br>
+            </p>
+        </div>
+        </div>
+    </div>
+    <div class="stage" style="background-color: #d6ccc2;">
+        <div class="fade-in">
+        <div class="title-section main-font">
+            <h1>고등학교 졸업장</h1>
+        </div>
+        <div class="title-caption caption-font">
+            <p style="text-align: center;">
+                지원 중 가장 먼저 준비해야 하는 서류는 고등학교 졸업장입니다. <br>
+            </p>
+        </div>
+        <div class="content-question third-font">
+            <h4 style="text-decoration: underline;">졸업 예정 증명서?</h4>
+            <p class="caption-font">
+                2021년 지원 당시 고등학교 졸업 예정이었기 때문에 졸업 예정 증명서를 교무실에서 영문으로 발급 받아 제출하였으나, <br>
+                UHR에서 decline 결정을 받았고 전화로 문의를 했었으나, 승인 받지 못했었습니다. <br>
+                따라서, 졸업 예정 증명서는 제출할 수 없습니다. <br>
+            </p>
+        </div>
+        <div class="content-question third-font">
+            <h4 style="text-decoration: underline;">마감일 전까지 졸업장을 받지 못한다면?</h4>
+            <p class="caption-font">
+                스웨덴 비자 혹은 적법한 유럽연합 비자가 없는 경우, <a href="https://www.universityadmissions.se/intl/start" target="_blank" style="color: black;">round 1에 지원해야 하나</a>, <br>
+                한국은 졸업이 2월달이기 때문에 서류 제출 마감일인 1월 중순까지 고등학교 졸업장을 제출하기 어려움이 있을 수 있습니다. <br>
+                다만, 학교장 재량에 따라 졸업장을 미리 받아볼 수 있으므로 공손하게 부탁드릴 것을 추천합니다. <br>
+            </p>
+        </div>
+        <div class="content-question third-font">
+            <h4 style="text-decoration: underline;">지원 가능한 고등학교 종류는?</h4>
+            <p class="caption-font">
+                UHR 웹사이트에는 인문계 고등학교 (일반고, 자사고) 또는 실업계 고등학교를 졸업한 학생에게 지원 자격이 있다고 명시되어 있습니다. <br>
+                만약 과학고, 영재고, 외고를 졸업하였다면, UHR에 문의하는 것을 추천드립니다.
+            </p>
+        </div>
+        </div>
+    </div>
+    <div class="stage" style="background-color: #e3d5ca;">
+        <div class="fade-in">
+        <div class="title-section main-font">
+            <h1>영어 조건</h1>
+        </div>
+        <div class="title-caption caption-font">
+            <p style="text-align: center;">
+                대한민국은 영어가 공용어가 아닌 국가에 해당하기 때문에, 반드시 국제적으로 공인된 어학능력 시험을 치뤄야 합니다. <br>
+            </p>
+        </div>
+        <div class="content-question third-font">
+            <h4 style="text-decoration: underline;">공인된 어학 능력 시험</h4>
+            <p class="caption-font">
+                UHR에 승인 받은 어학 능력 시험은 IELTS, TOEFL, University of Michigan ECPE, Pearson PTE, Cambridge ESOL 입니다.
+            </p>
+            <ul>
+                <li>
+                    IELTS: IELTS 시험 중 IELTS Academic, IELTS Online, IELTS UKVI만 승인 되었고 IELTS General Training과 IELTS Indicator는 승낙되지 않습니다.
+                    <ul>
+                        <li>English 7: 전체 점수 7.0이상 및 6.0 이하인 부문이 없음</li>
+                        <li>English 6: 전체 점수 6.5이상 및 5.5 이하인 부문이 없음</li>
+                    </ul>
+                </li>
+                <li>
+                    TOEFL: TOEFL 시험 중 TOEFL iBT, TOEFL iBT Home Edition, TOEFL iBT Paper Edition만 승인 되었고 TOEFL ITP, TOEFL iBT MyBest, Institutional TOEFL과 TOEFL Essentials는 승낙되지 않습니다.
+                    <ul>
+                        <li>English 7: 전체 점수 100이상 및 쓰기 부문 20점 이상</li>
+                        <li>English 6: 전체 점수 90이상 및 쓰기 부문 20점 이상</li>
+                    </ul>
+                </li>
+                <li>
+                    Michigan ECPE: ECPE의 경우 English 6를 얻기 위해서만 치룰 수 있는 시험이며 English 7 요건을 맞출 수 없습니다.
+                </li>
+                <li>
+                    Pearson PTE: Pearson PTE Academic과 PTE Academic Online 시험이 승인되어 있습니다.
+                    <ul>
+                        <li>English 7: 전체 점수 68점 이상 및 쓰기 부문 61점 이상</li>
+                        <li>English 6: 전체 점수 62점 이상 및 쓰기 부문 61점 이상</li>
+                    </ul>
+                </li>
+                <li>
+                    Cambridge ESOL:
+                    <ul>
+                        <li>English 7: C2 Proficiency (CPE), 최소 점수 200점 이상 혹은 Proficiency 등급</li>
+                        <li>English 6: C1 Advanced (CAE), 최소 점수 180점 이상 혹은 Advanced 등급</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        </div>
+    </div>
+    <div class="stage" style="background-color: #d6ccc2;">
+        <div class="fade-in">
+        <div class="title-section main-font">
+            <h1>여권 사본</h1>
+        </div>
+        <div class="title-caption caption-font">
+            <p style="text-align: center;">
+                여권의 증명 사진이 있는 페이지를 스캔하여 제출하면 됩니다.
+            </p>
+        </div>
+        </div>
+    </div>
+    <div class="stage" style="background-color: #e3d5ca;">
+        <div class="fade-in">
+        <div class="title-section main-font">
+            <h1>과거 대학교 기록 (선택사항)</h1>
+        </div>
+        <div class="title-caption caption-font">
+            <p style="text-align: center;">
+                대학교 기록은 고등학교 졸업장 조건을 대신할 수는 없습니다. <br>
+                하지만, 과거에 다른 대학교를 다닌 기록을 제출할 수 있다면, 몇몇 학과 또는 과목에 대한 선수강 요건 충족 조건으로 인정될 수 있습니다. 
+            </p>
+        </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.stage {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.explanation-wrapper {
+    display: flex;
+    flex-direction: row;
+}
+
+.explanation-box {
+    width: 35em;
+    height: 25em;
+    border-radius: 1.5em;
+    background-color: #e3d5ca;
+    margin: 1em;
+    margin-top: 2em;
+    display: flex;
+    flex-direction: column;
+}
+
+.title-section {
+    display: flex;
+    justify-content: center;
+}
+
+.box-header {
+    margin: 1.5em;
+    display: flex;
+    flex-direction: column;
+}
+
+.content-question {
+    margin: 4em;
+}
+
+.fade-in {
+    opacity: 0;
+    transition: opacity 1s ease-in-out;
+  }
+
+.fade-in-visible {
+    opacity: 1;
+}
+</style>
+<script>
+export default {
+    methods: {
+        observeElements() {
+        const options = {
+            threshold: 0.1
+        };
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('fade-in-visible')
+                }
+            });
+        }, options);
+
+        const elements = document.querySelectorAll('.fade-in');
+        elements.forEach(e1 => {
+            observer.observe(e1);
+        });
+      }
+    },
+    mounted() {
+        this.observeElements()
+    }
+}
+</script>
