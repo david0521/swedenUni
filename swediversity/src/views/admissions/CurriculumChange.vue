4bd93706768201594fafeb68c08797bc3dcb4dda
David Hong <davidbor@net.chalmers.se>
2024-07-18 23:13:36 +0200

    #53 National curriculum request page  This commit will create a page to explain on how to change the judgement criteria written on UHR.

---
+++ swediversity/src/views/admissions/CurriculumChange.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="section">
+        <div class="wrapper">
+            <div class="title-section">
+                <h1 class="main-font">UHR 공인 한국 교육과정 수정</h1>
+                <p style="text-align: center;">
+                    UHR에 등록되어 있는 한국 교육과정은 한국에서 교육과정이 변경 되더라도 지원자가 요청하지 않는 한 개정된 교육과정으로 교체 되지 않습니다. <br>
+                    2021년 말 지원 당시에도 UHR 교육과정을 확인한 결과, UHR에서 한국 학생을 평가하는 기준은 제7차 (2005년 개정) 교육과정이었습니다. <br>
+                    2021년 말 한국에서는 부분 개정된 2015년 교육과정을 따르고 있었기 때문에 UHR에 평가 기준 수정을 요청해야 했고, <br>
+                    훗날 한국 교육과정이 개정된다면 어떻게 대처할 수 있는지 참고할 수 있도록 당시 작성한 문서를 업로드 하였습니다. <br> 
+                    <br><br>
+                    <a href="https://drive.google.com/file/d/1SYyeiM_EEa03EvknigM5ymo1apt4-4Hg/view?usp=sharing" target="_blank" style="color: black;">파일 열기</a> 
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.title-section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
